2c213c6f578ff1912f24cf5b47bfa56653fc778d
alinachinfy <alinachinfy@gmail.com>
2021-02-19 20:43:10 +0900

    Initial commit

---
+++ src/Adept.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Adept extends JFrame {
+    Adept(){
+        setBounds(400, 400, 250, 300);
+        JButton inc = new JButton("+1");
+        JButton dec = new JButton("-1");
+        JTextField num = new JTextField("0");
+        num.setEditable(false);
+
+        JLabel label = new JLabel("i");
+        inc.setBounds(120, 20, 50, 20);
+        dec.setBounds(120, 40, 50, 20);
+        num.setBounds(60, 20, 30, 20);
+        label.setBounds(20, 20, 30, 20);
+
+        inc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int i = Integer.parseInt(num.getText());
+                if(i<3) i++;
+                num.setText(Integer.toString(i));
+            }
+        });
+
+        dec.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int i = Integer.parseInt(num.getText());
+                if(i>-5) i--;
+                num.setText(Integer.toString(i));
+            }
+        });
+        
+        add(inc);
+        add(dec);
+        add(num);
+        add(label);
+        setVisible(true);
+    }
+    public static void main(String[] args) {new Adept();}
+}
